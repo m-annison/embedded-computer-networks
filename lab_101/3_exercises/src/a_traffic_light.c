91c4252bfe28405c3bc93779c829088f6954bd15
m.annison <b7014596@my.shu.ac.uk>
2019-10-03 10:52:00 +0100

    Answered exercises

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -15,6 +15,10 @@
 
 // map the led to gpio pins
 gpio_pin_t led1 = {PA_8,  GPIOA, GPIO_PIN_8};
+gpio_pin_t ledRed = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t ledAmb = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t ledGre = {PB_15, GPIOB, GPIO_PIN_15};
+
 
 // this is the main method
 int main()
@@ -23,5 +27,28 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
+	
+	init_gpio(led1, OUTPUT);
+	init_gpio(ledRed, OUTPUT);
+	init_gpio(ledAmb, OUTPUT);
+	init_gpio(ledGre, OUTPUT);
+	
+	
+	while(1){
+	
+		write_gpio(ledRed, HIGH);
+		HAL_Delay(2000);
+		write_gpio(ledAmb, HIGH);
+		HAL_Delay(500);
+		toggle_gpio(ledRed);
+		toggle_gpio(ledAmb);
+		write_gpio(ledGre, HIGH);
+		HAL_Delay(3000);
+		toggle_gpio(ledGre);
+		toggle_gpio(ledAmb);
+		HAL_Delay(1000);
+		toggle_gpio(ledAmb);
+	}
+	
 
 }
--- lab_101/3_exercises/src/b_led_dice.c
+++ lab_101/3_exercises/src/b_led_dice.c
@@ -16,6 +16,14 @@
 
 // map the led to gpio pins
 gpio_pin_t led1 = {PA_8,  GPIOA, GPIO_PIN_8};
+gpio_pin_t ledUL = {PC_7,  GPIOC, GPIO_PIN_7};
+gpio_pin_t ledL = {PC_6,  GPIOC, GPIO_PIN_6};
+gpio_pin_t ledM = {PG_6,  GPIOG, GPIO_PIN_6};
+gpio_pin_t ledBL = {PI_3,  GPIOI, GPIO_PIN_3};
+gpio_pin_t ledUR = {PG_7,  GPIOG, GPIO_PIN_7};
+gpio_pin_t ledR = {PI_0,  GPIOI, GPIO_PIN_0};
+gpio_pin_t ledBR = {PH_6,  GPIOH, GPIO_PIN_6};
+
 
 // this is the main method
 int main()
@@ -24,5 +32,73 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
+	
+	init_gpio(ledUL, OUTPUT);
+	init_gpio(ledL, OUTPUT);
+	init_gpio(ledBL, OUTPUT);
+	init_gpio(ledM, OUTPUT);
+	init_gpio(ledUR, OUTPUT);
+	init_gpio(ledR, OUTPUT);
+	init_gpio(ledBR, OUTPUT);
+	
+	init_random();
+	
+	uint32_t rnd = (get_random_int() % 6) + 1;
+	
 
+	
+	if(rnd == 1){
+		write_gpio(ledUL, LOW);
+		write_gpio(ledL, LOW);
+		write_gpio(ledBL, LOW);
+		write_gpio(ledM, HIGH);
+		write_gpio(ledUR, LOW);
+		write_gpio(ledR, LOW);
+		write_gpio(ledBR, LOW);
+	}
+	else if (rnd == 2){
+			write_gpio(ledUL, HIGH);
+		write_gpio(ledL, LOW);
+		write_gpio(ledBL, LOW);
+		write_gpio(ledM, LOW);
+		write_gpio(ledUR, LOW);
+		write_gpio(ledR, LOW);
+		write_gpio(ledBR, HIGH);
+	}
+	else if (rnd == 3){
+			write_gpio(ledUL, HIGH);
+		write_gpio(ledL, LOW);
+		write_gpio(ledBL, LOW);
+		write_gpio(ledM, HIGH);
+		write_gpio(ledUR, LOW);
+		write_gpio(ledR, LOW);
+		write_gpio(ledBR, HIGH);
+	}
+	else if (rnd == 4){
+			write_gpio(ledUL, HIGH);
+		write_gpio(ledL, LOW);
+		write_gpio(ledBL, HIGH);
+		write_gpio(ledM, LOW);
+		write_gpio(ledUR, HIGH);
+		write_gpio(ledR, LOW);
+		write_gpio(ledBR, HIGH);
+	}
+	else if (rnd == 5){
+			write_gpio(ledUL, HIGH);
+		write_gpio(ledL, LOW);
+		write_gpio(ledBL, HIGH);
+		write_gpio(ledM, HIGH);
+		write_gpio(ledUR, HIGH);
+		write_gpio(ledR, LOW);
+		write_gpio(ledBR, HIGH);
+	}
+	else if (rnd == 6) {
+				write_gpio(ledUL, HIGH);
+		write_gpio(ledL, HIGH);
+		write_gpio(ledBL, HIGH);
+		write_gpio(ledM, LOW);
+		write_gpio(ledUR, HIGH);
+		write_gpio(ledR, HIGH);
+		write_gpio(ledBR, HIGH);
+	}
 }
